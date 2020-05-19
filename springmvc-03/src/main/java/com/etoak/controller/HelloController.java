81608fa44be2b8ae40833d063cb15768040c4434
etoak <etoak@et.com>
2020-05-19 15:01:36 +0800

    重构maven的pom.xml,提交springmvc-02和springmvc-03

---
+++ springmvc-03/src/main/java/com/etoak/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.etoak.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@RequestMapping("index")
+	public String index(HttpServletRequest request) {
+		String name = request.getParameter("name");
+		System.out.println("param name " + name);
+		
+		//向request域传值
+		request.setAttribute("result", "hello " + name);
+		return "hello/index";
+	}
+}
